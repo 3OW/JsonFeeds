d50bebc169903f2cbcc1cc40e6b3183a33e30f59
3OW <mail@lehmannfelix.de>
2024-07-15 23:33:23 +0200

    change defaullt port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { RssModule } from './rss.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RssModule);
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 80);
 }
 bootstrap();
