91737b07d3ca754e8a425ab8f7caa23b2dcd9127
3OW <mail@lehmannfelix.de>
2024-07-16 20:31:33 +0200

    added new feed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { RssModule } from './rss.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RssModule);
-  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 80);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
